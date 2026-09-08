40e8255d5ed3b3d715d73251ed9283faa6c85fc2
Steve Singer <steve@ssinger.info>
2026-09-08 17:54:57 -0400

    Minor updates to the sponsorship page.

--- site/(main)/+page.svelte
+++ site/(main)/+page.svelte
@@ -151,9 +151,9 @@
   <div class="(main)">
     <hgroup class="margin-gap">
       <p>Sponsors</p>
-      <h2>The companies underwriting 2027.</h2>
+      <h2>The companies Sponsoring 2027.</h2>
       <p class="text">
-        PGConf.dev is non-profit. Sponsors cover the venue, A/V, scholarships,
+        PGConf.dev is non-profit. Sponsors help cover the venue, A/V, scholarships,
         and the parts of the program that ticket revenue alone can't.
       </p>
     </hgroup>
